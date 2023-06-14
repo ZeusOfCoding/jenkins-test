eb5d91c66d205af6a640e13c1db76acda164cf3c
L <blaisepaho@gmail.com>
2023-06-14 13:21:59 +0100

    spring couldn't find the controller. So i move it under the main package. Hope it will solve the problem

--- src/main/java/com/pnb/jenkinstest/controllers/HelloController.java
+++ src/main/java/com/pnb/jenkinstest/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.pnb.controllers;
+package com.pnb.jenkinstest.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
